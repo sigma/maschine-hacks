52c50c2b1bc87b9a40732acc67d5146387acf445
Yann Hodique <yhodique@google.com>
2022-12-05 22:52:12 +0100

    flake: add overlay

--- flake.nix
+++ flake.nix
@@ -4,21 +4,14 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachSystem [
-      flake-utils.lib.system.aarch64-darwin
-      flake-utils.lib.system.x86_64-darwin
-    ] (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        name = "maschine-hacks";
-      in rec {
-        defaultPackage = packages.maschine-hacks;
-        packages.maschine-hacks = pkgs.stdenv.mkDerivation {
-          pname = name;
-          version = "0.1";
-          src = ./.;
+    let
+      name = "maschine-hacks";
+      pkg = pkgs: pkgs.stdenv.mkDerivation {
+        pname = name;
+        version = "0.1";
+        src = ./.;
 
-          buildPhase = ''
+        buildPhase = ''
             cat <<EOF > maschine-patch
               #!/bin/sh
               cd "/Applications/Native Instruments/Maschine 2/Maschine 2.app/Contents/Resources/Scripts/Maschine"
@@ -35,14 +28,28 @@
               patch -R -p1 < $src/installer.patch
             EOF
           '';
-          dontUnpack = true;
+        dontUnpack = true;
 
-          installPhase = ''
+        installPhase = ''
             mkdir -p $out/bin
             install -m 755 maschine-patch $out/bin/maschine-patch
             install -m 755 maschine-unpatch $out/bin/maschine-unpatch
           '';
-        };
-      }
-    );
+      };
+    in
+      flake-utils.lib.eachSystem [
+        flake-utils.lib.system.aarch64-darwin
+        flake-utils.lib.system.x86_64-darwin
+      ] (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in rec {
+          defaultPackage = packages.maschine-hacks;
+          packages.maschine-hacks = pkg pkgs;
+        }
+      ) // {
+        overlays.default = (final: prev: {
+          maschine-hacks = pkg prev;
+        });
+      };
 }
